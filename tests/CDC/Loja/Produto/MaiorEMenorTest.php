0ae96b0b4ad06cb2fbe19c78dadc9690fd49e3ad
Rafael Queiroz <rafael.castro6@fatec.sp.gov.br>
2018-09-02 15:06:28 -0300

    Testes automatizados funcionando devidamente

--- tests/CDC/Loja/Produto/MaiorEMenorTest.php
+++ tests/CDC/Loja/Produto/MaiorEMenorTest.php
@@ -10,20 +10,42 @@ use CDC\Loja\Produto\MaiorEMenor;
 
 use PHPUnit_Framework_TestCase as PHPUnit;
 
-class MaiorEMenor extends PHPUnit
+class TestaMaiorEMenor extends PHPUnit
 {
     public function testOrdemDecrescente()
     {
         $carrinho = new CarrinhoDeCompras();
 
-        $carrinho->adiciona( new Produto("Geladeira", 450.00));
-        $carrinho->adiciona( new Produto("Liquidificador", 250.00));
-        $carrinho->adiciona( new Produto("Jogo de Pratos", 70.00));
+        /**
+         * O exemplo do livro retorna um erro indicando que a instancia do objeto não corresponde
+         * ao que está sendo recebido.
+         */
+        // $carrinho->adiciona(new Produto("Geladeira", 450.00));
+        // $carrinho->adiciona(new Produto("Liquidificador", 250.00));
+        // $carrinho->adiciona(new Produto("Jogo de Pratos", 70.00));
+        $carrinho->produtos->append(new Produto("Geladeira", 450.00));
+        $carrinho->produtos->append(new Produto("Liquidificador", 250.00));
+        $carrinho->produtos->append(new Produto("Jogo de Pratos", 70.00));
 
         $maiorMenor = new MaiorEMenor();
         $maiorMenor->encontra($carrinho);
 
         $this->assertEquals("Jogo de Pratos", $maiorMenor->getMenor()->getNome());
+        $this->assertEquals("Geladeira", $maiorMenor->getMaior()->getNome());
+        $this->assertInstanceOf("CDC\Loja\Produto\Produto", $maiorMenor->getMenor());
+
+    }
+
+    public function testApenasUmProduto()
+    {
+        $carrinho = new CarrinhoDeCompras();
+
+        $carrinho->produtos->append(new Produto("Geladeira", 450.00));
+
+        $maiorMenor = new MaiorEMenor();
+        $maiorMenor->encontra($carrinho);
+
+        $this->assertEquals("Geladeira", $maiorMenor->getMenor()->getNome());
         $this->assertEquals("Geladeira", $maiorMenor->getMaior()->getNome());
     }
 }
